71da9f585664ec59f57b50d58d6228afd0262a5d
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-03-23 13:59:33 +0100

    Update keyframes.ts

--- src/utils/keyframes.ts
+++ src/utils/keyframes.ts
@@ -16,16 +16,16 @@ export const mushroomKeyframes = css`
   
   /* SCREEN FLICKER EFFECT - use with :before-element and content:""; - adjust width, height and margin to your needs */
   @keyframes flicker { 
-    0% { background: linear-gradient(180deg, rgba(166, 40, 235, 0.3) 0%, transparent 30%, transparent 100%); }
-    25% { background: linear-gradient(180deg, transparent 0%, rgba(166, 40, 235, 0.3) 25%, transparent 55%, transparent 100%); }
-    50% { background: linear-gradient(180deg, transparent 0%, transparent 20%, rgba(166, 40, 235, 0.3) 50%, transparent 80%, transparent 100%); }
-    75% { background: linear-gradient(180deg, transparent 0%, transparent 45%, rgba(166, 40, 235, 0.3) 75%, transparent 100%); }
+    0%   { background: linear-gradient(180deg, rgba(166, 40, 235, 0.3) 0%, transparent 30%, transparent 100%); }
+    25%  { background: linear-gradient(180deg, transparent 0%, rgba(166, 40, 235, 0.3) 25%, transparent 55%, transparent 100%); }
+    50%  { background: linear-gradient(180deg, transparent 0%, transparent 20%, rgba(166, 40, 235, 0.3) 50%, transparent 80%, transparent 100%); }
+    75%  { background: linear-gradient(180deg, transparent 0%, transparent 45%, rgba(166, 40, 235, 0.3) 75%, transparent 100%); }
     100% { background: linear-gradient(180deg, transparent 0%, transparent 70%, rgba(166, 40, 235, 0.3) 100%); }
   }
   
   /* HEATING RADIATOR - use with mdi:radiator */
   @keyframes heat {
-   0% {clip-path: inset(50% 0 0 0); }
+   0%        { clip-path: inset(50% 0 0 0); }
    95%, 100% { clip-path: inset(0 0 0 0); }
   }
   
@@ -43,30 +43,30 @@ export const mushroomKeyframes = css`
   
   /* RINGING BELL – use with mdi:bell-ring */
   @keyframes ring {
-    0% { transform: rotate(0); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    2% { transform: rotate(30deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    6% { transform: rotate(-28deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    10% { transform: rotate(34deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    14% { transform: rotate(-32deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    18% { transform: rotate(30deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    22% { transform: rotate(-28deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    26% { transform: rotate(26deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    30% { transform: rotate(-24deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    34% { transform: rotate(22deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    38% { transform: rotate(-20deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    42% { transform: rotate(18deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    46% { transform: rotate(-16deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    50% { transform: rotate(14deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    54% { transform: rotate(-12deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    58% { transform: rotate(10deg); clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
-    62% { transform: rotate(-8deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
-    66% { transform: rotate(6deg); }
-    70% { transform: rotate(-4deg); }
-    74% { transform: rotate(2deg); }
-    78% { transform: rotate(-1deg); }
-    82% { transform: rotate(1deg); }
-    86% { transform: rotate(0); }
-    100% { transform: rotate(0); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    0%   { transform: rotate(0);      clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    2%   { transform: rotate(30deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    6%   { transform: rotate(-28deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    10%  { transform: rotate(34deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    14%  { transform: rotate(-32deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    18%  { transform: rotate(30deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    22%  { transform: rotate(-28deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    26%  { transform: rotate(26deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    30%  { transform: rotate(-24deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    34%  { transform: rotate(22deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    38%  { transform: rotate(-20deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    42%  { transform: rotate(18deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    46%  { transform: rotate(-16deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    50%  { transform: rotate(14deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    54%  { transform: rotate(-12deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    58%  { transform: rotate(10deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%); }
+    62%  { transform: rotate(-8deg);  clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
+    66%  { transform: rotate(6deg); }
+    70%  { transform: rotate(-4deg); }
+    74%  { transform: rotate(2deg); }
+    78%  { transform: rotate(-1deg); }
+    82%  { transform: rotate(1deg); }
+    86%  { transform: rotate(0); }
+    100% { transform: rotate(0);      clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
   }
   
   /* ANIMATED VACUUM – use with mdi:robot-vacuum */
@@ -108,28 +108,28 @@ export const mushroomKeyframes = css`
   
   /* ANIMATED WASHING MACHINE 1 - SHAKE and BUBBLE - use with mdi:washing-machine and local:wash */
   @keyframes shake {
-    0%, 100% { transform: translate(0, 0) rotate(0); }
-    20%  { transform: translate(0.4px, -0.4px) rotate(-4deg); }
-    40%  { transform: translate(-0.4px, 0.4px) rotate(4deg); }
-    60%  { transform: translate(0.4px, 0.4px) rotate(-4deg); }
-    80%  { transform: translate(-0.4px, -0.4px) rotate(4deg); }
+    0%, 100% { transform: translate(0, 0)           rotate(0); }
+    20%      { transform: translate(0.4px, -0.4px)  rotate(-4deg); }
+    40%      { transform: translate(-0.4px, 0.4px)  rotate(4deg); }
+    60%      { transform: translate(0.4px, 0.4px)   rotate(-4deg); }
+    80%      { transform: translate(-0.4px, -0.4px) rotate(4deg); }
   }
   @keyframes bubble {
-    0%, 100%  {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 28% 63%, 31% 47%, 43% 37%, 65% 40%, 73% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    12.5%     {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 51% 42%, 64% 40%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    25%       {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 60% 51%, 69% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    37.5%     {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 64% 59%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    50%       {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 48% 61%, 58% 56%, 64% 59%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    62.5%     {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 46% 46%, 52% 47%, 63% 58%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    75%       {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 46% 46%, 52% 47%, 58% 53%, 67% 46%, 73% 62%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
-    87.5%     {clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 45% 40%, 57% 52%, 67% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    0%, 100%  { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 28% 63%, 31% 47%, 43% 37%, 65% 40%, 73% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    12.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 51% 42%, 64% 40%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    25%       { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 60% 51%, 69% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    37.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 64% 59%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    50%       { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 48% 61%, 58% 56%, 64% 59%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    62.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 46% 46%, 52% 47%, 63% 58%, 71% 56%, 72% 66%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    75%       { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 34% 60%, 43% 57%, 46% 46%, 52% 47%, 58% 53%, 67% 46%, 73% 62%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
+    87.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 45% 40%, 57% 52%, 67% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);}
   }
   /* ANIMATED WASHING MACHINE 2 - WASH and RINSE-DRY - use with local:waves and mdi:dishwasher or mdi:tumble-dryer */
   @keyframes wash {
-    0%, 100% { transform: translateX(20%); clip-path: circle(21.7% at 30% 58%); }
-    30% { transform: translateX(-15%); clip-path: circle(21.7% at 65% 58%); }
-    45% { transform: translateX(10%); clip-path: circle(21.7% at 40% 58%); }
-    75% { transform: translateX(-25%); clip-path: circle(21.7% at 75% 58%);}
+    0%, 100% { transform: translateX(20%);  clip-path: circle(21.7% at 30% 58%); }
+    30%      { transform: translateX(-15%); clip-path: circle(21.7% at 65% 58%); }
+    45%      { transform: translateX(10%);  clip-path: circle(21.7% at 40% 58%); }
+    75%      { transform: translateX(-25%); clip-path: circle(21.7% at 75% 58%);}
   }
   @keyframes rinse-dry {
     50%  { clip-path:  inset(100% 0 0 0); }
@@ -137,9 +137,9 @@ export const mushroomKeyframes = css`
   
   /* ANIMATED DISHWASHER - use with mdi:dishwasher */
   @keyframes bounce {
-    0%, 20%, 50%, 80%, 100% {transform: translateY(0); } 
-    40% { transform: translateY(-1.2px) rotate(5deg); } 
-    60% { transform: translateY(-1.1px) rotate(-4deg); } 
+    0%, 20%, 50%, 80%, 100% { transform: translateY(0); } 
+    40%                     { transform: translateY(-1.2px) rotate(5deg); } 
+    60%                     { transform: translateY(-1.1px) rotate(-4deg); } 
   } 
   @keyframes dishwash {
     50%  { clip-path: polygon(0 0, 0 100%, 35% 100%, 36% 74%, 31% 43%, 61% 40%, 71% 69%, 62% 78%, 36% 73%, 35% 100%, 100% 100%, 100% 0); }
@@ -147,16 +147,16 @@ export const mushroomKeyframes = css`
 
  /* COURIOUS DOG - use with local:frenchie (or any other face-like icon) */
  @keyframes huh {
-    0%, 10%, 75%, 100% { transform: rotate(0deg); }
-    15% { transform: rotate(-25deg); }
-    30%, 35% { transform: rotate(-35deg); }
-    50% { transform: rotate(12deg); }
-    65% { transform: rotate(-8deg); }
+    0%, 10%, 75%, 100% { transform: rotate(0deg);   }
+    15%                { transform: rotate(-25deg); }
+    30%, 35%           { transform: rotate(-35deg); }
+    50%                { transform: rotate(12deg);  }
+    65%                { transform: rotate(-8deg);  }
  }
 
  /* AIR-ANIMATION - use with local:air-freshener */
  @keyframes air {
-    100% { clip-path: inset(0% 0 0 0); }
+    100% { clip-path: inset(0% 0 0 0);  }
     50%  { clip-path: inset(35% 0 0 0); }
  }
 `;
