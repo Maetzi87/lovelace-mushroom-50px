4a57deb7fc4bceb81d9d66a8825a65bb083229f0
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-03-25 11:08:28 +0100

    Repair resize-card.ts

--- src/cards/resize-card/resize-card.ts
+++ src/cards/resize-card/resize-card.ts
@@ -1,4 +1,443 @@
-econdary-color": this.getValue("secondary_text_color"),
+import { UnsubscribeFunc } from "home-assistant-js-websocket";
+import { css, html, LitElement, nothing, PropertyValues } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import { styleMap } from "lit/directives/style-map.js";
+import memoizeOne from "memoize-one";
+import hash from "object-hash/dist/object_hash";
+import {
+  actionHandler,
+  ActionHandlerEvent,
+  atLeastHaVersion,
+  computeDomain,
+  DOMAINS_TOGGLE,
+  handleAction,
+  hasAction,
+  HomeAssistant,
+  LovelaceCard,
+  LovelaceCardEditor,
+  LovelaceGridOptions,
+  RenderTemplateResult,
+  subscribeRenderTemplate,
+  ActionHandlerOptions,
+} from "../../ha";
+import { computeCssColor } from "../../ha/common/color/compute-color";
+import { isTemplate } from "../../ha/common/string/has-template";
+import { CacheManager } from "../../utils/cache-manager";
+import { registerCustomCard } from "../../utils/custom-cards";
+import { ResizeCardConfig } from "./resize-card-config";
+import { getWeatherSvgIcon } from "../../utils/icons/weather-icon";
+import { weatherSVGStyles } from "../../utils/weather";
+import { mushroomKeyframes } from "../../utils/keyframes";
+
+export const getEntityDefaultTileIconAction = (entityId: string) => {
+  const domain = computeDomain(entityId);
+  const supportsIconAction =
+    DOMAINS_TOGGLE.has(domain) ||
+    ["button", "input_button", "scene"].includes(domain);
+
+  return supportsIconAction ? "toggle" : "none";
+};
+
+registerCustomCard({
+  type: "mushroomic-resize-card",
+  name: "Mushroomic Resize",
+  description: "Build your own Mushroom card with templates, including size and styling options",
+});
+
+const templateCache = new CacheManager<TemplateResults>(1000);
+
+type TemplateResults = Partial<
+  Record<TemplateKey, RenderTemplateResult | undefined>
+>;
+
+const TEMPLATE_KEYS = [
+  "icon",
+  "color",
+  "primary",
+  "secondary",
+  "picture",
+  "badge_icon",
+  "badge_color",
+  "badge_text",
+  
+  // Sizes
+  "tile_size",
+  "icon_size",
+  "badge_size",
+  "badge_icon_size",
+  "badge_icon_color",
+
+  // Primary text
+  "primary_text_size",
+  "primary_text_weight",
+  "primary_text_color",
+  "primary_line_height",
+  "primary_letter_spacing",
+
+  // Secondary text
+  "secondary_text_size",
+  "secondary_text_weight",
+  "secondary_text_color",
+  "secondary_line_height",
+  "secondary_letter_spacing",
+] as const;
+
+
+type TemplateKey = (typeof TEMPLATE_KEYS)[number];
+
+export interface LovelaceCardFeatureContext {
+  entity_id?: string;
+  area_id?: string;
+}
+
+@customElement("mushroomic-resize-card")
+export class MushroomicResizeCard extends LitElement implements LovelaceCard {
+  public static async getConfigElement(): Promise<LovelaceCardEditor> {
+    await import("./resize-card-editor");
+    return document.createElement(
+      "mushroomic-resize-card-editor"
+    ) as LovelaceCardEditor;
+  }
+
+  public static getStubConfig(): ResizeCardConfig {
+    return {
+      type: `custom:mushroomic-resize-card`,
+      primary: "Hello, {{user}}",
+      secondary: "How are you?",
+      icon: "mdi:mushroom",
+    };
+  }
+
+  @property({ attribute: false }) public hass?: HomeAssistant;
+
+  @state() private _config?: ResizeCardConfig;
+
+  @state() private _templateResults?: TemplateResults;
+
+  @state() private _unsubRenderTemplates: Map<
+    TemplateKey,
+    Promise<UnsubscribeFunc>
+  > = new Map();
+
+  public connectedCallback() {
+    super.connectedCallback();
+    this._tryConnect();
+  }
+
+  public disconnectedCallback() {
+    super.disconnectedCallback();
+    this._tryDisconnect();
+
+    if (this._config && this._templateResults) {
+      const key = this._computeCacheKey();
+      templateCache.set(key, this._templateResults);
+    }
+  }
+
+  private _computeCacheKey() {
+    return hash(this._config);
+  }
+
+  protected willUpdate(_changedProperties: PropertyValues): void {
+    super.willUpdate(_changedProperties);
+    if (!this._config) {
+      return;
+    }
+
+    if (!this._templateResults) {
+      const key = this._computeCacheKey();
+      if (templateCache.has(key)) {
+        this._templateResults = templateCache.get(key)!;
+      } else {
+        this._templateResults = {};
+      }
+    }
+  }
+
+  protected updated(changedProps: PropertyValues): void {
+    super.updated(changedProps);
+    if (!this._config || !this.hass) {
+      return;
+    }
+
+    this._tryConnect();
+  }
+
+  private _getTemplateKeyValue(key: TemplateKey): string {
+    if (!this._config) {
+      return "";
+    }
+    return this._config[key] ?? "";
+  }
+
+  private async _tryConnect(): Promise<void> {
+    TEMPLATE_KEYS.forEach((key) => {
+      this._tryConnectKey(key);
+    });
+  }
+
+  private async _tryConnectKey(key: TemplateKey): Promise<void> {
+    if (
+      this._unsubRenderTemplates.get(key) !== undefined ||
+      !this.hass ||
+      !this._config
+    ) {
+      return;
+    }
+
+    const value = this._getTemplateKeyValue(key);
+    if (!isTemplate(value)) {
+      return;
+    }
+    try {
+      const sub = subscribeRenderTemplate(
+        this.hass.connection,
+        (result) => {
+          this._templateResults = {
+            ...this._templateResults,
+            [key]: result,
+          };
+        },
+        {
+          template: value,
+          entity_ids: this._config.entity_id,
+          variables: {
+            config: this._config,
+            user: this.hass.user!.name,
+            entity: this._config.entity,
+            area: this._config.area,
+          },
+          strict: true,
+        }
+      );
+      this._unsubRenderTemplates.set(key, sub);
+      await sub;
+    } catch (_err) {
+      const result = {
+        result: this._config[key] ?? "",
+        listeners: {
+          all: false,
+          domains: [],
+          entities: [],
+          time: false,
+        },
+      };
+      this._templateResults = {
+        ...this._templateResults,
+        [key]: result,
+      };
+      this._unsubRenderTemplates.delete(key);
+    }
+  }
+  private async _tryDisconnect(): Promise<void> {
+    TEMPLATE_KEYS.forEach((key) => {
+      this._tryDisconnectKey(key);
+    });
+  }
+
+  private async _tryDisconnectKey(key: TemplateKey): Promise<void> {
+    const unsubRenderTemplate = this._unsubRenderTemplates.get(key);
+    if (!unsubRenderTemplate) {
+      return;
+    }
+
+    try {
+      const unsub = await unsubRenderTemplate;
+      unsub();
+      this._unsubRenderTemplates.delete(key);
+    } catch (err: any) {
+      if (err.code === "not_found" || err.code === "template_error") {
+        // If we get here, the connection was probably already closed. Ignore.
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  public setConfig(config: ResizeCardConfig): void {
+    this._config = {...config};
+
+    if (this._config.entity) {
+      if (!this._config.tap_action) {
+        this._config.tap_action = { action: "more-info" };
+      }
+      if (!this._config.icon_tap_action) {
+        this._config.icon_tap_action = {
+          action: getEntityDefaultTileIconAction(this._config.entity),
+        };
+      }
+    }
+  }
+
+  private _featureContext = memoizeOne(
+    (config: ResizeCardConfig): LovelaceCardFeatureContext => {
+      return {
+        entity_id: config.entity,
+        area_id: config.area,
+      };
+    }
+  );
+
+  private getValue(key: TemplateKey) {
+    const value = this._getTemplateKeyValue(key);
+    return isTemplate(value)
+      ? this._templateResults?.[key]?.result?.toString()
+      : value;
+  }
+
+  public getCardSize(): number {
+    const featuresPosition =
+      this._config && this._featurePosition(this._config);
+    const featuresCount = this._config?.features?.length || 0;
+
+    const hasContent = Boolean(
+      this._config?.icon ||
+        this._config?.picture ||
+        this._config?.primary ||
+        this._config?.secondary
+    );
+
+    return (
+      (hasContent || featuresPosition === "inline" ? 1 : 0) +
+      (this._config?.vertical ? 1 : 0) +
+      (featuresPosition === "inline" ? 0 : featuresCount)
+    );
+  }
+
+  public getGridOptions(): LovelaceGridOptions {
+    let columns: number | undefined = 6;
+    let rows: number | undefined = 0;
+
+    const hasContent = Boolean(
+      this._config?.icon ||
+        this._config?.picture ||
+        this._config?.primary ||
+        this._config?.secondary
+    );
+
+    rows = hasContent ? 1 : 0;
+
+    const featurePosition = this._config && this._featurePosition(this._config);
+    const featuresCount = this._config?.features?.length || 0;
+    if (featuresCount) {
+      if (featurePosition === "inline") {
+        columns = 12;
+        rows = 1;
+      } else {
+        rows += featuresCount;
+      }
+    }
+
+    if (this._config?.vertical) {
+      if (
+        this._config.primary ||
+        (this._config.secondary && !this._config.icon)
+      ) {
+        rows++;
+      }
+    }
+    if (this._config?.multiline_secondary) {
+      rows = undefined;
+    }
+    return {
+      columns,
+      rows,
+    };
+  }
+
+  private _handleAction(ev: ActionHandlerEvent) {
+    handleAction(this, this.hass!, this._config!, ev.detail.action!);
+  }
+
+  private _handleIconAction(ev: CustomEvent) {
+    ev.stopPropagation();
+    const config = {
+      entity: this._config!.entity,
+      tap_action: this._config!.icon_tap_action,
+      hold_action: this._config!.icon_hold_action,
+      double_tap_action: this._config!.icon_double_tap_action,
+    };
+    handleAction(this, this.hass!, config, ev.detail.action!);
+  }
+
+  private get _hasCardAction() {
+    return (
+      hasAction(this._config?.tap_action) ||
+      hasAction(this._config?.hold_action) ||
+      hasAction(this._config?.double_tap_action)
+    );
+  }
+
+  private get _hasIconAction() {
+    return (
+      hasAction(this._config?.icon_tap_action) ||
+      hasAction(this._config?.icon_hold_action) ||
+      hasAction(this._config?.icon_double_tap_action)
+    );
+  }
+
+  private _featurePosition = memoizeOne((config: ResizeCardConfig) => {
+    if (config.vertical) {
+      return "bottom";
+    }
+    return config.features_position || "bottom";
+  });
+
+  private _displayedFeatures = memoizeOne((config: ResizeCardConfig) => {
+    const features = config.features || [];
+    const featurePosition = this._featurePosition(config);
+
+    if (featurePosition === "inline") {
+      return features.slice(0, 1);
+    }
+    return features;
+  });
+
+  protected render() {
+    if (!this._config || !this.hass) {
+      return nothing;
+    }
+
+    const icon = this.getValue("icon");
+    const color = this.getValue("color");
+    const cssColor = color ? computeCssColor(color) : undefined;
+    const primary = this.getValue("primary");
+    const secondary = this.getValue("secondary");
+    const picture = this.getValue("picture");
+    const badgeIcon = this.getValue("badge_icon");
+    const badgeColor = this.getValue("badge_color");
+    const badgeText = this.getValue("badge_text");
+    const badgeCssColor = badgeColor ? computeCssColor(badgeColor) : undefined;
+    const badgeIconColor = this.getValue("badge_icon_color");
+
+    const weatherSvg = getWeatherSvgIcon(icon);
+    
+// --- Resize variables (template-capable) ---
+    const tileSize = this.getValue("tile_size");
+    const iconSize = this.getValue("icon_size");
+    const badgeSize = this.getValue("badge_size");
+    const badgeIconSize = this.getValue("badge_icon_size");
+    
+// --- Automatic fallback scaling ---
+    const finalTileSize = tileSize || "50px";
+    const finalIconSize = iconSize || `calc(${finalTileSize} * 0.66)`;
+    const finalBadgeSize = badgeSize || `calc(${finalTileSize} * 0.32)`;
+    const finalBadgeIconSize = badgeIconSize || `calc(${finalBadgeSize} * 0.75)`;
+    
+    const style = {
+      "--tile-color": cssColor,
+    
+      "--ha-tile-info-primary-font-size": this.getValue("primary_text_size"),
+      "--ha-tile-info-primary-font-weight": this.getValue("primary_text_weight"),
+      "--ha-tile-info-primary-color": this.getValue("primary_text_color"),
+      "--ha-tile-info-primary-line-height": this.getValue("primary_line_height"),
+      "--ha-tile-info-primary-letter-spacing": this.getValue("primary_letter_spacing"),
+
+      "--ha-tile-info-secondary-font-size": this.getValue("secondary_text_size"),
+      "--ha-tile-info-secondary-font-weight": this.getValue("secondary_text_weight"),
+      "--ha-tile-info-secondary-color": this.getValue("secondary_text_color"),
       "--ha-tile-info-secondary-line-height": this.getValue("secondary_line_height"),
       "--ha-tile-info-secondary-letter-spacing": this.getValue("secondary_letter_spacing"),
     };
