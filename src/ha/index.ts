28ccfac97ac9b7740830f4335894e59bae40e1a2
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-03 01:07:12 +0200

    Add export for action-struct from editor

--- src/ha/index.ts
+++ src/ha/index.ts
@@ -3,5 +3,6 @@ export * from "./common/entity/compute_domain";
 export * from "./panels/lovelace/card-features/types";
 export * from "./panels/lovelace/common/directives/action-handler-directive";
 export * from "./panels/lovelace/common/has-action";
+export * from "./panels/lovelace/editor/structs/action-struct";
 export * from "./types";
 export * from "./util";
