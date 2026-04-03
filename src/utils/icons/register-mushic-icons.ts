c9ff3a6d5f3f8d8765d7ebb2b006e1eb0d1d29ca
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-03 21:48:28 +0200

    Create register-mushic-icons.ts

---
+++ src/utils/icons/register-mushic-icons.ts
@@ -0,0 +1,17 @@
+import { customIconset } from "../../ha";
+import { mushicIcons } from "./mushic-icons";
+
+customIconset("mushic", {
+  async getIcon(name: string) {
+    const path = mushicIcons[name];
+    if (!path) return undefined;
+
+    try {
+      const resp = await fetch(path);
+      if (!resp.ok) return undefined;
+      return await resp.text();
+    } catch {
+      return undefined;
+    }
+  }
+});
