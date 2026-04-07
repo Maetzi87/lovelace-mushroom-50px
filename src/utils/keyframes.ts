a735cddf7bbdf0eabaeadcde796a15b3b1ffa122
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-07 15:24:40 +0200

    Update keyframes.ts

--- src/utils/keyframes.ts
+++ src/utils/keyframes.ts
@@ -2,12 +2,17 @@ import { css } from "lit";
 
 export const mushroomicKeyframes = css`
   /* ALERT – makes any icon blink */
-  @keyframes blink {
+  @keyframes mushic-blink {
     100% { opacity: 0; }
+  }
+  /* ALERT – makes shape blink red */
+  @keyframes mushic-ping {
+    0%   { box-shadow: 0 0 0 0 rgba(var(--rgb-red), 0.7); }
+    100% { box-shadow: 0 0 5px 7px transparent;           }
   }
 
   /* BATTERY CHARGING ANIMATION – use with mdi:battery-high */
-  @keyframes charge {
+  @keyframes mushic-charge {
     0%  { clip-path: polygon(0% 0%, 0% 100%, 37% 100%, 37% 28%, 65% 28%, 65% 80%, 37% 80%, 37% 100%, 100% 100%, 100% 0%); }
     20% { clip-path: polygon(0% 0%, 0% 100%, 37% 100%, 37% 28%, 65% 28%, 65% 65%, 37% 64%, 37% 100%, 100% 100%, 100% 0%); }
     40% { clip-path: polygon(0% 0%, 0% 100%, 37% 100%, 37% 28%, 65% 28%, 65% 45%, 37% 45%, 37% 100%, 100% 100%, 100% 0%); }
@@ -15,7 +20,7 @@ export const mushroomicKeyframes = css`
   }
   
   /* SCREEN FLICKER EFFECT - use with :before-element and content:""; - adjust width, height and margin to your needs */
-  @keyframes flicker { 
+  @keyframes mushic-flicker { 
     0%   { background: linear-gradient(180deg, rgba(166, 40, 235, 0.3) 0%, transparent 30%, transparent 100%);                                   }
     25%  { background: linear-gradient(180deg, transparent 0%, rgba(166, 40, 235, 0.3) 25%, transparent 55%, transparent 100%);                  }
     50%  { background: linear-gradient(180deg, transparent 0%, transparent 20%, rgba(166, 40, 235, 0.3) 50%, transparent 80%, transparent 100%); }
@@ -24,25 +29,25 @@ export const mushroomicKeyframes = css`
   }
   
   /* HEATING RADIATOR - use with mdi:radiator */
-  @keyframes heat {
+  @keyframes mushic-heat {
    0%        { clip-path: inset(50% 0 0 0); }
    95%, 100% { clip-path: inset(0 0 0 0);   }
   }
   
   /* OPENING DOOR – use with mdi:door */
-  @keyframes open {
+  @keyframes mushic-door {
     0%, 66% { transform: rotateY(0deg);    transform-origin: 30%; }
     33%     { transform: rotateY(-120deg); transform-origin: 30%; }
   }
 
   /* SEND ANIMATION – use with mdi:access-point */
-  @keyframes send {
+  @keyframes mushic-send {
     0%   { clip-path: circle(13% at 50% 50%); }
     100% { clip-path: circle(50% at 50% 50%); }
   }
   
   /* RINGING BELL – use with mdi:bell-ring */
-  @keyframes ring {
+  @keyframes mushic-ring {
     0%   { transform: rotate(0);      clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
     2%   { transform: rotate(30deg);  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);                                                        }
     6%   { transform: rotate(-28deg); clip-path: polygon(0 50%, 0 100%, 100% 100%, 100% 50%, 85% 50%, 80% 30%, 60% 5%, 40% 5%, 20% 30%, 15% 50%); }
@@ -70,7 +75,7 @@ export const mushroomicKeyframes = css`
   }
   
   /* ANIMATED VACUUM – use with mdi:robot-vacuum */
-  @keyframes vacuum {
+  @keyframes mushic-vacuum {
     0%          { transform: translate(0, 0) rotate(0);           }
     5%, 7%      { transform: rotate(-40deg);                      }
     12%, 14%    { transform: rotate(40deg);                       }
@@ -87,34 +92,40 @@ export const mushroomicKeyframes = css`
   }
 
   /* ANIMATED CEILING FAN 1 – WIND - use with local:center-and-wind */
-  @keyframes wind-forward {
+  @keyframes mushic-wind-forward {
     0%        { clip-path: polygon(0% 60%, 50% 73%, 100% 60%, 100% 0%, 0% 0%); }
     50%       { clip-path: inset(0 0% 30% 0);                                  }
     95%, 100% { clip-path: inset(0 0 0 0);                                     }
   }
-  @keyframes wind-reverse {
+  @keyframes mushic-wind-reverse {
     0%, 5%  { clip-path: inset(0% 0% 0% 0%);                                 }
     50%     { clip-path: inset(0 0% 30% 0);                                  }
     100%    { clip-path: polygon(0% 60%, 50% 73%, 100% 60%, 100% 0%, 0% 0%); }
   }
 
   /* ANIMATED CEILING FAN 2 – ROTATION - use with local:blades */
-  @keyframes blade-rotation {
+  @keyframes mushic-blade-rotation {
     0%, 100%  { transform: rotateY(0deg);    }
     25%       { transform: rotateY(-90deg);  }
     50%       { transform: rotateY(-180deg); }
     75%       { transform: rotateY(-90deg);  }
   }
+
+  /* ANIMATED FAN (or whatever) - makes icon rotate */
+  @keyframes mushic-rotate {
+    0%   { transform: rotate(0deg);   }
+    100% { transform: rotate(360deg); }
+  }
   
   /* ANIMATED WASHING MACHINE 1 - SHAKE and BUBBLE - use with mdi:washing-machine and local:wash */
-  @keyframes shake {
+  @keyframes mushic-shake {
     0%, 100% { transform: translate(0, 0)           rotate(0);     }
     20%      { transform: translate(0.4px, -0.4px)  rotate(-4deg); }
     40%      { transform: translate(-0.4px, 0.4px)  rotate(4deg);  }
     60%      { transform: translate(0.4px, 0.4px)   rotate(-4deg); }
     80%      { transform: translate(-0.4px, -0.4px) rotate(4deg);  }
   }
-  @keyframes bubble {
+  @keyframes mushic-bubble {
     0%, 100%  { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 28% 63%, 31% 47%, 43% 37%, 65% 40%, 73% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);                            }
     12.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 51% 42%, 64% 40%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%); }
     25%       { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 40% 50%, 48% 61%, 58% 56%, 60% 51%, 69% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%); }
@@ -125,28 +136,28 @@ export const mushroomicKeyframes = css`
     87.5%     { clip-path: polygon(0% 0%, 0% 100%, 35% 100%, 34% 74%, 27% 60%, 33% 46%, 45% 40%, 57% 52%, 67% 47%, 74% 61%, 63% 77%, 47% 81%, 34% 74%, 35% 100%, 100% 100%, 100% 0%);                   }
   }
   /* ANIMATED WASHING MACHINE 2 - WASH and RINSE-DRY - use with local:waves and mdi:dishwasher or mdi:tumble-dryer */
-  @keyframes wash {
+  @keyframes mushic-wash {
     0%, 100% { transform: translateX(20%);  clip-path: circle(21.7% at 30% 58%); }
     30%      { transform: translateX(-15%); clip-path: circle(21.7% at 65% 58%); }
     45%      { transform: translateX(10%);  clip-path: circle(21.7% at 40% 58%); }
     75%      { transform: translateX(-25%); clip-path: circle(21.7% at 75% 58%); }
   }
-  @keyframes rinse-dry {
+  @keyframes mushic-rinse-dry {
     50%  { clip-path:  inset(100% 0 0 0); }
   }
   
   /* ANIMATED DISHWASHER - use with mdi:dishwasher */
-  @keyframes bounce {
+  @keyframes mushic-bounce {
     0%, 20%, 50%, 80%, 100% { transform: translateY(0);                    } 
     40%                     { transform: translateY(-1.2px) rotate(5deg);  } 
     60%                     { transform: translateY(-1.1px) rotate(-4deg); } 
   } 
-  @keyframes dishwash {
+  @keyframes mushic-dishwash {
     50%  { clip-path: polygon(0 0, 0 100%, 35% 100%, 36% 74%, 31% 43%, 61% 40%, 71% 69%, 62% 78%, 36% 73%, 35% 100%, 100% 100%, 100% 0); }
  }
 
  /* COURIOUS DOG - use with local:frenchie (or any other face-like icon) */
- @keyframes huh {
+ @keyframes mushic-huh {
     0%, 10%, 75%, 100% { transform: rotate(0deg);   }
     15%                { transform: rotate(-25deg); }
     30%, 35%           { transform: rotate(-35deg); }
@@ -155,13 +166,13 @@ export const mushroomicKeyframes = css`
  }
 
  /* AIR-ANIMATION - use with local:air-freshener */
- @keyframes air {
+ @keyframes mushic-air {
     100% { clip-path: inset(0% 0 0 0);  }
     50%  { clip-path: inset(35% 0 0 0); }
  }
 
 /* ANIMATED PURIFIER - use with mdi: air-purifier */
- @keyframes purify {
+ @keyframes mushic-purify {
     0%, 100% { clip-path: inset(0 0 0 0);                                                                }
     25%      { clip-path: polygon(100% 100%, 0 100%, 0 0, 100% 0, 98% 32%, 63% 42%, 65% 58%, 100% 43%);  }
     75%      { clip-path: polygon(100% 100%, 0 100%, 0 0, 100% 0, 100% 44%, 64% 61%, 64% 73%, 100% 72%); }
@@ -169,18 +180,18 @@ export const mushroomicKeyframes = css`
  }
  
  /* SIGNAL-ANIMATION - use with mdi:wifi */
- @keyframes good_signal {
+ @keyframes mushic-good-signal {
     0%  { clip-path: circle(0% at 50% 85%);  }
     20% { clip-path: circle(30% at 50% 85%); }
     40% { clip-path: circle(55% at 50% 85%); }
     60% { clip-path: circle(80% at 50% 85%); }
  }
- @keyframes ok_signal {
+ @keyframes mushic-ok-signal {
     0%  { clip-path: circle(0% at 50% 85%);  }
     20% { clip-path: circle(30% at 50% 85%); }
     40% { clip-path: circle(55% at 50% 85%); }
  }
- @keyframes low_signal {
+ @keyframes mushic-low-signal {
     0%  { clip-path: circle(0% at 50% 85%);  }
     20% { clip-path: circle(30% at 50% 85%); }
  }
